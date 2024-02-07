657ecff5b385e30c4aece7db0e0984d4dace675a
kenizaya <harveylstark@gmail.com>
2024-02-07 15:12:08 +0530

    Implement empty cart state

---
+++ src/components/cart.tsx
@@ -0,0 +1,26 @@
+import { useStore } from '@nanostores/solid';
+import { Show, createSignal } from 'solid-js';
+import { $cart, removeItemFromCart, subtotal } from '../stores/cart';
+import styles from './cart.module.css';
+
+function formatCurrency(amount: number) {
+	return new Intl.NumberFormat('en-US', {
+		currency: 'usd',
+		style: 'currency',
+	}).format(amount);
+}
+
+const EmptyState = () => {
+	return (
+		<>
+			<p class={styles.icon}>
+				<span role="img" aria-label="hot-dog">
+					🌭
+				</span>
+			</p>
+			<p class={styles.empty}>
+				Your cart is empty! Add a sandwich kit or two and give flavor a chance.
+			</p>
+		</>
+	);
+};
