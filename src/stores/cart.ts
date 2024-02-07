9a157fe53de3591abf9948b002c83d469d002578
kenizaya <harveylstark@gmail.com>
2024-02-07 14:49:04 +0530

    Implement addItemToCart

---
+++ src/stores/cart.ts
@@ -0,0 +1,14 @@
+import { computed, map } from 'nanostores';
+import type { CartItem, ShopItem } from '../types';
+
+export const $cart = map<Record<number, CartItem>>({});
+
+export function addItemToCart(item: ShopItem) {
+	const cartItem = $cart.get()[item.id];
+	const quantity = cartItem ? cartItem.quantity : 0;
+
+	$cart.setKey(item.id, {
+		item,
+		quantity: quantity + 1,
+	});
+}
